a23a163edcc60fc186fd42a211313f99882ccccf
rakhmanov <rakhmanov.1995@mail.ru>
2024-10-31 01:52:39 +0300

    feat: Add hosts CURS swaggers

--- hosts.js
+++ hosts.js
@@ -12,7 +12,30 @@ const hosts = [
 
   // stands
   'curs-root-ui.dev.curs.apps.innodev.local',
-  'api-gw.dev.curs.apps.innodev.local'
+  'api-gw.dev.curs.apps.innodev.local',
+
+  // CURS swaggers
+  'contractor.dev.curs.apps.innodev.local',
+  'compendium.dev.curs.apps.innodev.local',
+  'curs-opsnext.dev.curs.apps.innodev.local',
+  'deals-and-limits.dev.curs.apps.innodev.local',
+  'deals-and-limits-integration.dev.curs.apps.innodev.local',
+  'decision.dev.curs.apps.innodev.local',
+  'dfbs.dev.curs.apps.innodev.local',
+  'dfbs-control.dev.curs.apps.innodev.local',
+  'dfbs-mb.dev.curs.apps.innodev.local',
+  'document-search.dev.curs.apps.innodev.local',
+  'etl-integration.dev.curs.apps.innodev.local',
+  'incident.dev.curs.apps.innodev.local',
+  'kantor-curs.dev.curs.apps.innodev.local',
+  'monitoring.dev.curs.apps.innodev.local',
+  'migrate-spkp-curs.dev.curs.apps.innodev.local',
+  'notification.dev.curs.apps.innodev.local',
+  'orchestrator.dev.curs.apps.innodev.local',
+  'osfp-integration.dev.curs.apps.innodev.local',
+  'pp-integration.dev.curs.apps.innodev.local',
+  'report.dev.curs.apps.innodev.local',
+  'role-model.dev.curs.apps.innodev.local',
 ]
 
 const options = { family: 4 };
